aec2e72c68116daa5da85c3bee20bb1158c0ca1c
Matheus Nunes <matheus.silva@compasso.com.br>
2022-03-24 09:40:27 -0300

    feat: create subject entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TeacherModule } from './teacher/teacher.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot()],
+  imports: [TypeOrmModule.forRoot(), TeacherModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/subject/entities/subject.entity.ts
@@ -0,0 +1,31 @@
+import { Teacher } from 'src/teacher/entities/teacher.entity';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class Subject {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ unique: true })
+  name: string;
+
+  @CreateDateColumn({ default: () => 'CURRENT_TIMESTAMP(6)', select: false })
+  createdAt: Date;
+
+  @UpdateDateColumn({
+    default: () => 'CURRENT_TIMESTAMP(6)',
+    onUpdate: 'CURRENT_TIMESTAMP(6)',
+    select: false,
+  })
+  updatedAt: Date;
+
+  @ManyToOne(() => Teacher, (teacher) => teacher.subjects)
+  teacher: Teacher;
+}

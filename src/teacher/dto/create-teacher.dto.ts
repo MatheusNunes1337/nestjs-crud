0465386528661003bfe81566b5d749b271974409
Matheus Nunes <matheus.silva@compasso.com.br>
2022-03-24 15:59:00 -0300

    feat: create create-teacher-dto class validator

--- src/teacher/dto/create-teacher.dto.ts
+++ src/teacher/dto/create-teacher.dto.ts
@@ -1,1 +1,37 @@
-export class CreateTeacherDto {}
+import {
+  IsDate,
+  IsDateString,
+  IsNotEmpty,
+  IsString,
+  Length,
+  Matches,
+  IsArray,
+  ArrayUnique,
+} from 'class-validator';
+
+interface Subject {
+  name: string;
+}
+
+export class CreateTeacherDto {
+  @IsString()
+  @Length(3, 15)
+  @IsNotEmpty()
+  name: string;
+
+  @IsString()
+  @Length(3, 15)
+  @IsNotEmpty()
+  lastname: string;
+
+  @Matches(/^\d{3}\.\d{3}\.\d{3}-\d{2}$/)
+  @IsNotEmpty()
+  cpf: string;
+
+  @IsDateString()
+  birthdate: Date;
+
+  @IsArray()
+  @ArrayUnique()
+  subjects: Subject[];
+}

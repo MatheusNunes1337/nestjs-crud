aa6ad438deeb13bb34eccc1e20939e5bcf193281
Matheus Nunes <matheus.silva@compasso.com.br>
2022-04-01 09:12:54 -0300

    feat: config swagger ui in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,22 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+
+  const options = new DocumentBuilder()
+    .setTitle('Nest API')
+    .setDescription('NestJS API')
+    .setVersion('1.0')
+    .addTag('school')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api-docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
